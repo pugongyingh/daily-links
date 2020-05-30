51a6a1f75dd2b0176ba0e3e4c50c6277a548fc2d
pugongyingh <pgyhh@sina.cn>
2020-05-30 10:22:10 +0800

    Update createUser.ts

--- src/functions/createUser.ts
+++ src/functions/createUser.ts
@@ -1,6 +1,5 @@
-import faunadb from 'faunadb';
-import jwt from 'jsonwebtoken';
-
+const faunadb = require('faunadb');
+const jwt = require('jsonwebtoken');
 // your secret hash
 const secret = `fnADs5ccBTACCm5kbmjncetPrz6o9t2bqV5gQvZl`;
 export const q = faunadb.query;
@@ -8,7 +7,7 @@ export const q = faunadb.query;
 export const client = new faunadb.Client({ secret });
 
 export const verifyToken = (token) => jwt.verify(token, process.env.jwt_secret);
-import jwt from 'jsonwebtoken';
+
 
 module.exports = async (req, res) => {
     try {
