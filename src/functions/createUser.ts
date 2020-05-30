7ce604bf9951072b305541df1e6b7c0880947191
pugongyingh <pgyhh@sina.cn>
2020-05-30 10:03:52 +0800

    Update createUser.ts

--- src/functions/createUser.ts
+++ src/functions/createUser.ts
@@ -12,6 +12,34 @@ import jwt from 'jsonwebtoken';
 
 module.exports = async (req, res) => {
     try {
+      
+    try {
+
+              let body;
+        try{
+                body = JSON.parse(req.body);
+        }catch(ex){
+                body = event.body;
+        }
+
+        let username = body.username;
+        let password = body.password;
+   // const { username, password } = event.body;
+
+    /** @type { { data: { username: string, password: string } } }  */
+   // const user = await client.query(
+  //    q.Get(q.Match(q.Index('users_by_username'), username)),
+ //   );
+ //   if (user == null) {
+    /** @type { { data: { username: string } } }  */
+    const user = await client.query(
+      q.Create(q.Collection('users'), {
+        data: { username, password },
+      }),
+    );
+ //   }
+  var token = jwt.sign(req.body, 'shhhhh');
+  //context.succeed(token);           
         // let res;
         // try {
         //     res = await client.query(
@@ -29,13 +57,13 @@ module.exports = async (req, res) => {
             })
         );
         // ok
-        const user = {
-            userId: dbs?.data?.id,
-            login: dbs?.data?.login,
-            name: dbs?.data?.name,
-        };
-        const token = jwt.sign({ user }, process.env.jwt_secret);
-        res.status(200).json({ ...user, token });
+     //   const user = {
+     //       userId: dbs.data.id,
+     //       login: dbs.data.login,
+     //       name: dbs.data.name,
+      //  };
+      //  const token = jwt.sign({ user }, process.env.jwt_secret);
+        res.status(200).json({ token });
     } catch (e) {
         // something went wrong
         res.status(500).json({ error: e.message });
