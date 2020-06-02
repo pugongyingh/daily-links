4ced22eb60be94c458fc3bd0f85d19db7b80e897
pugongyingh <pgyhh@sina.cn>
2020-06-02 18:46:25 +0800

    Update create.js

--- src/functions/create.js
+++ src/functions/create.js
@@ -73,7 +73,7 @@ q.Get(q.Match(q.Index('users_by_username'), username))
   return {
     status: 200,
     type: 'text/html; charset=utf8',
-    body: JSON.stringify({token: token}),
+    body: '<h1>Hello world!555</h1>',
     cors: true,
   }
     }
