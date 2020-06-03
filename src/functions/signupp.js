34fcfc9693c9b8042656c9872c822a484965e6d9
pugongyingh <pgyhh@sina.cn>
2020-06-03 18:12:32 +0800

    Update signupp.js

--- src/functions/signupp.js
+++ src/functions/signupp.js
@@ -1,8 +1,9 @@
 // @ts-check
 const { app, registerDefaultErrorHandler } = require('./bootstrap/app');
 const awsServerlessExpress = require('aws-serverless-express');
-const faunadb = require('faunadb');
 const bcrypt = require('bcryptjs');
+const faunadb = require('faunadb');
+const jwt = require('jsonwebtoken');
 
 const q = faunadb.query;
 
@@ -14,20 +15,28 @@ app.post('/.netlify/functions/signupp', async (req, res, next) => {
   try {
     const { username, password } = req.body;
 
-    console.log(
-      await client.query(q.Get(q.Match(q.Index('users_by_username'), 'jake'))),
-    );
-
-    /** @type { { data: { username: string } } }  */
+    /** @type { { data: { username: string, password: string } } }  */
     const user = await client.query(
       q.Create(q.Collection('users'), {
         data: { username, password) },
       }),
     );
-    res.json({
-      user: {
-        username: username,
+    if (user == null) {
+      return next(new Error('User not found'));
+    }
+
+    const token = jwt.sign(
+      {
+        username: user.data.username,
       },
+      'secret',
+      {
+        expiresIn: '1h',
+      },
+    );
+
+    res.json({
+      token,
     });
   } catch (error) {
     console.error(error);
