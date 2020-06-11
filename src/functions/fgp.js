6d48472355a35def465a29eb700999162b909e7a
pugongyingh <pgyhh@sina.cn>
2020-06-11 19:37:19 +0800

    Update fgp.js

--- src/functions/fgp.js
+++ src/functions/fgp.js
@@ -4,70 +4,36 @@ const awsServerlessExpress = require('aws-serverless-express');
 const bcrypt = require('bcryptjs');
 const faunadb = require('faunadb');
 const jwt = require('jsonwebtoken');
-const nodemailer = require("nodemailer");
+
 const q = faunadb.query;
 
 const client = new faunadb.Client({
   secret: `fnADs5ccBTACCm5kbmjncetPrz6o9t2bqV5gQvZl`,
 });
 
-  
 app.post('/.netlify/functions/fgp', async (req, res, next) => {
   try {
-    const { username} = req.body;
+    const { username, password } = req.body;
 
     /** @type { { data: { username: string, password: string } } }  */
     const user = await client.query(
-      q.Get(q.Match(q.Index('users_by_username'), username)),
+      q.Create(q.Collection('users'), {data: { username, password},}),
     );
 
-    if (user == null) {
-     // return next(new Error('User not found'));
-          res.json({
-      username,
-    });
-    }
 
-    const transport = nodemailer.createTransport({
-    host: "smtp.sina.com", // 主机
-    secureConnection: true, // 使用 SSL
-    port: 465, // SMTP 端口
-    auth: {
-        user: "mit777@sina.com",
-        pass: "48946dc4ad709a34"
-
-    }
-    });
-
-    //const { email }  = JSON.parse(event.body) 
-    let mailOptions = {
-      from: "mit777@sina.com",
-      to: user.data.username,
-      subject: "888",
-      text: "999",
-  };
     const token = jwt.sign(
       {
-        username: user.data.username,
+        username: username,
       },
       'secret',
       {
         expiresIn: '1h',
       },
     );
-const url = `http://127.0.0.1:8076/dy/change-password.html?` + token;
-    transporter.sendMail({
-      to: user.data.username,
-      subject: "Confirm email",
-      html: `Please click this link to confirm your email: <a href=${url}>${url}</a>`
-    });
 
     res.json({
       token,
     });
-    
-
-
   } catch (error) {
     console.error(error);
     next(error);
