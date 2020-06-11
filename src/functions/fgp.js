646bd520a897d2b473184510887e5b0520c471c4
pugongyingh <pgyhh@sina.cn>
2020-06-11 21:05:01 +0800

    Update fgp.js

--- src/functions/fgp.js
+++ src/functions/fgp.js
@@ -29,7 +29,52 @@ exports.handler = async (event,context,callback) => {
       q.Get(q.Match(q.Index('users_by_username'), username)),
     );
 
-     
+
+    if (user == null) {
+  return {
+    status: 200,
+    type: 'text/html; charset=utf8',
+    body: '<h1>Hello world4470007!'  +'</h1>',
+    cors: true,
+  } 
+    }
+
+    const transport = nodemailer.createTransport({
+    host: "smtp.sina.com", // 主机
+    secureConnection: true, // 使用 SSL
+    port: 465, // SMTP 端口
+    auth: {
+        user: "mit777@sina.com",
+        pass: "48946dc4ad709a34"
+
+    }
+    });
+
+    //const { email }  = JSON.parse(event.body) 
+    let mailOptions = {
+      from: "mit777@sina.com",
+      to: user.data.username,
+      subject: "888",
+      text: "999",
+  };
+    const token = jwt.sign(
+      {
+        username: username,
+      },
+      'secret',
+      {
+        expiresIn: '1h',
+      },
+    );
+const url = `http://127.0.0.1:8076/dy/change-password.html?` + token;
+    transporter.sendMail({
+      to: username,
+      subject: "Confirm email",
+      html: `Please click this link to confirm your email: <a href=${url}>${url}</a>`
+    });
+
+      
+      
       
   return {
     status: 200,
