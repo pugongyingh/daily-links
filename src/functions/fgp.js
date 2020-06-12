3053d5683536162f01f98ba74554404019477b71
pugongyingh <pgyhh@sina.cn>
2020-06-12 10:47:44 +0800

    Update fgp.js

--- src/functions/fgp.js
+++ src/functions/fgp.js
@@ -37,51 +37,20 @@ exports.handler = async (event,context,callback) => {
     body: '<h1>Hello world4470007!' +username +'</h1>',
     cors: true,
   } 
-    }
-
-    const transport = nodemailer.createTransport({
-    host: "smtp.sina.com", // 主机
-    secureConnection: true, // 使用 SSL
-    port: 465, // SMTP 端口
-    auth: {
-        user: "mit777@sina.com",
-        pass: "48946dc4ad709a34"
-
-    }
-    });
-
-    //const { email }  = JSON.parse(event.body) 
-    let mailOptions = {
-      from: "mit777@sina.com",
-      to: username,
-      subject: "888",
-      text: "999",
-  };
-    const token = jwt.sign(
-      {
-        username: username,
-      },
-      'secret',
-      {
-        expiresIn: '1h',
-      },
-    );
-const url = `http://127.0.0.1:8076/dy/change-password.html?` + token;
-    transport.sendMail({
-      to: username,
-      subject: "Confirm email",
-      html: `Please click this link to confirm your email: <a href=${url}>${url}</a>`
-    });
-
-      
-      
-      
-  return {
+    }else{
+      return {
     status: 200,
     type: 'text/html; charset=utf8',
     body: '<h1>Hello world4477!'  +username+'</h1>',
     cors: true,
-  }      
+  } 
+    }
+
+
+      
+      
+      
+     
     } catch (err) { 
         const error = {
             status: err.status || 500,
