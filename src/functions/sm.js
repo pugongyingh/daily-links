698c8827ba88c7654202a4790861950e18d48a9a
pugongyingh <pgyhh@sina.cn>
2020-06-15 21:04:28 +0800

    Update sm.js

--- src/functions/sm.js
+++ src/functions/sm.js
@@ -5,7 +5,7 @@
 //const result = require('dotenv').config()
 const nodemailer = require('nodemailer');
 
-const mailUtils = require("../mail");
+
 export async function handler(event, context, callback){
  // const { user, pass} = process.env
  const  body = JSON.parse(event.body);
@@ -13,8 +13,8 @@ export async function handler(event, context, callback){
    // const max = 99999;
    // const num = Math.floor(Math.random() * (max - min + 1)) + min;
 var  mm = process.env.mm;
-var  tmp;
- var  sub;
+var  tmp ="88";
+ var  sub = "99";
   var  ss = parseInt(body.send);
   var  tt = parseInt(body.tmp);
   //var  str = body.name;
@@ -26,45 +26,6 @@ var  tmp;
 
      mm =mm.split('!')[ss];
 
- 
-   switch (tt) {
-    case 1:
-      tmp = mailUtils.getMailBody1(body);
-      break;
-    case 2:
-      tmp = mailUtils.getMailBody2(body);
-      break;
-    case 3:
-      tmp = mailUtils.getMailBody3(body);
-      break;
-    case 4:
-      tmp = mailUtils.getMailBody4(body);
-      break;
-    case 5:
-      tmp = mailUtils.getMailBody5(body);
-      break;
-    default:
-      tmp = mailUtils.getMailBody1(body);
-  }
-   switch (tt) {
-    case 1:
-      sub = mailUtils.getMailSub1(body);
-      break;
-    case 2:
-      sub = mailUtils.getMailSub2(body);
-      break;
-    case 3:
-      sub = mailUtils.getMailSub3(body);
-      break;
-    case 4:
-      sub = mailUtils.getMailSub4(body);
-      break;
-    case 5:
-      sub = mailUtils.getMailSub5(body);
-      break;
-    default:
-      sub = mailUtils.getMailSub1(body);
-  }
  var  mmm=mm.split(';')[0];
  var  pp=mm.split(';')[1];
  var  sss = mmm.split('@')[1];
@@ -76,7 +37,7 @@ var  tmp;
           else
           {
               hh="smtp."+ sss;
-          }
+          };
     const transport = nodemailer.createTransport({
     host: hh, // 主机
     secureConnection: true, // 使用 SSL
@@ -105,15 +66,13 @@ try{
 // min=process.env.yyy;
   return {
     statusCode: 200,
-    body: min,
-    cors: true
+    body: min
   }
 }catch(err){
 //console.log(err)
   return {
     statusCode: 400,
-    body: "err",
-    cors: true
+    body: "err"
   };
 }
 
